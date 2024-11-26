eaf5dca6db4612a134ded7995456ea8cb7821590
jonathan.gobiel <jonathan.gobiel@bytedance.com>
2024-11-26 16:24:54 +0700

    feat: init repo

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import("next").NextConfig} nextConfig
+ */
+const nextConfig = {
+  /* config options here */
+  env: {
+    ENDPOINT: "https://api.pintu.co.id/v2",
+  },
+};
+
+export default nextConfig;
